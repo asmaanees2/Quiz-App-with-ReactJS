4a2eb5ea705f944e5f191781a37949d4309e42f7
asmaanees <anisasma195@gmail.com>
2023-02-04 02:31:56 +0500

    first commit

---
+++ src/Data/QuizData.js
@@ -0,0 +1,64 @@
+export const QuizData = [
+    {
+        question: " HTML stands for _______",
+        options: [
+            "Hyper  Makeup Language",
+            "Hyper  Markup Language",
+            "Hypertext  Makeup Language",
+            "Hypertext Markup Language",
+        ],
+        answer: 4
+    },
+    {
+        question: " CSS stands for _________",
+        options: [
+            "Casecading Src Sheet",
+            "Casecading Script Sheet",
+            "Casecading Stands Sheet",
+            "Casecading Style Sheet",
+        ],
+        answer: 4
+        // correctAns: "Casecading Style Sheet",
+    },
+    {
+        question: " JS stands for _________",
+        options: ["Java Style", "Java Script", "Job Script", "Java Src"],
+        answer: 2
+        // correctAns: "Java Script",
+      },
+      {
+        question: " DOM stands for _________",
+        options: [
+        "Document Object Model",
+         "Document Oriented Model", 
+         "Document Only Model", 
+         "Document One Model"
+        ],
+        answer: 1
+        // correctAns: "Document Object Model",
+      },
+      {
+        question: " RAM stands for _________",
+        options: ["Random Only Mode",
+        "Read Only Mode",
+        "Random Acccess Memory",
+        "Read Only Memory",],
+        answer: 3
+        // correctAns: "Random Acccess Memory",
+      },
+      {
+        question: " ROM stands for _________",
+        options: [
+          "Random Only Mode",
+          "Read Only Mode",
+          "Random Acccess Memory",
+          "Read Only Memory",
+        ],
+        answer: 4
+        // correctAns: "Read Only Memory",
+      },
+];
+
+
+
+
